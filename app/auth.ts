76d6772bd14e42d1d8d5ef2bad89ece1d8c45dee
Kenneth Rabina <kennethrabina400@gmail.com>
2026-07-12 02:30:22 +0800

    predeployment fix v6

--- app/auth.ts
+++ app/auth.ts
@@ -1,13 +1,11 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google" 
 
-import { prisma } from "@/lib/prisma"
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
   callbacks: {
     signIn ({ user }) {
       return user.email === process.env.ALLOWED_ADMIN_EMAIL
-      },
+    },
   },
 })
