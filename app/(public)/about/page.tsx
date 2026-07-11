9abf3bd99d3566ddc81579d5ea66a50f3bc357a5
Kenneth Rabina <kennethrabina400@gmail.com>
2026-07-12 03:56:55 +0800

    about me page changes

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -119,10 +119,10 @@ export default function Home() {
 
   return (
     
-    <main className="h-[calc(100vh-5rem)] font-raleway z-2" >
+    <main className="min-h-[calc(100vh-5rem)] font-raleway z-2" >
         
-        <div className="flex flex-row justify-between px-50 py-20">
-            <div className="sticky top-20 flex flex-col h-full w-full md:w-72">
+        <div className="flex flex-row justify-between max-w-6xl mx-auto gap-16 px-8 py-20">
+            <div className="sticky top-20 flex flex-col w-full md:w-72">
               <div className="flex flex-col h-full" >
                 <h1 className="text-5xl font-bold text-[#ffffff] mb-3 whitespace-nowrap">
                   Kenneth Rabina
@@ -152,7 +152,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
-          <div className="flex-1 flex flex-col px-15 mx-15 max-w-full overflow-hidden">
+          <div className="flex-1 min-w-0 flex flex-col max-w-2xl overflow-hidden">
             <section id ="aboutme" ref={aboutRef}> 
               <div className="mb-15 text-md">
                   <p>I’m a frontend engineer with an expertise in building accessible, pixel-perfect user interfaces. I take pride in crafting thoughtful, inclusive products and have a sharp eye for the little details that elevate user experience. I do my best work at the intersection of design and engineering, where great UX meets clean, scalable code.</p>
