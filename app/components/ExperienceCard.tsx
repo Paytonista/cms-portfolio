99e0896dfdd02f6839b6c77a89e5c6ee06bd8be3
Kenneth Rabina <kennethrabina400@gmail.com>
2026-07-13 22:27:57 +0800

    design bug fixes

--- app/components/ExperienceCard.tsx
+++ app/components/ExperienceCard.tsx
@@ -39,8 +39,8 @@ const ExperienceCard = ({
     >
       <div className="flex items-start gap-3 p-3 w-full min-w-0">
         <div className="w-24 shrink-0">
-          <div className="flex flex-col text-sm text-gray-400 whitespace-nowrap py-1 text-center text-sm">
-            {end_date ? <div>{start_date?.toLocaleDateString()} - {end_date?.toLocaleDateString()}</div> : <div>{start_date?.toLocaleDateString()} - Active</div>}
+          <div className="flex flex-col text-sm text-gray-400 whitespace-nowrap py-1 text-center ">
+            {end_date ? <div className="text-sm">{start_date?.toLocaleDateString()} - {end_date?.toLocaleDateString()}</div> : <div>{start_date?.toLocaleDateString()} - Active</div>}
               
              
           </div>
