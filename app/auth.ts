537caf170928e4ba8f4d53c96e50c6ecd69ddba0
Kenneth Rabina <kennethrabina400@gmail.com>
2026-07-14 18:05:35 +0800

    fixed google auth

--- app/auth.ts
+++ app/auth.ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google" 
+import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [Google],
   callbacks: {
-    signIn ({ user }) {
+    signIn({ user }) {
       return user.email === process.env.ALLOWED_ADMIN_EMAIL
     },
   },
