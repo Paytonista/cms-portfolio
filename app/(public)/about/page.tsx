f3100908658718896c08e0c0ca030865a14e40f4
Kenneth Rabina <kennethrabina400@gmail.com>
2026-07-15 15:09:13 +0800

    changes

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -137,9 +137,9 @@ export default function Home() {
                 <p className="text-[#989898] text-xs">Passionate about software development, technology, and creating user-focused applications. Always eager to learn, build, and grow as a developer.</p>
                 <div className=" flex flex-row order-2 w-full
                   lg:flex-col lg:mt-4 lg:order-1">
-                  <PortfolioButton isActive={activeSection === "aboutme"} onClick={() => scrollToSection("aboutme")} >About Me</PortfolioButton>
-                  <PortfolioButton isActive={activeSection === "experience"} onClick={() => scrollToSection("experience")}>Experiences</PortfolioButton>
-                  <PortfolioButton isActive={activeSection === "projects"} onClick={() => scrollToSection("projects")}>Projects</PortfolioButton>
+                  <PortfolioButton className="flex-1" isActive={activeSection === "aboutme"} onClick={() => scrollToSection("aboutme")} >About Me</PortfolioButton>
+                  <PortfolioButton className="flex-1" isActive={activeSection === "experience"} onClick={() => scrollToSection("experience")}>Experiences</PortfolioButton>
+                  <PortfolioButton className="flex-1" isActive={activeSection === "projects"} onClick={() => scrollToSection("projects")}>Projects</PortfolioButton>
                 </div>
                 <div className="order-1
                 lg:mt-50 lg:order-2">
